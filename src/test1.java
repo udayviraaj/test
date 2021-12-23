08d73c38873317fa8f13ce87332bc2cfebb5a160
udayviraaj <gudayviraaj1996@gmail.com>
2021-12-23 08:55:17 +0530

    added the syso command for the second line

---
+++ src/test1.java
@@ -0,0 +1,8 @@
+public class test1 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("uday");
+        System.out.println("this the first project importing to git repository");
+    }
+}
